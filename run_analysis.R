14e7d59c93bec5c6804f8aaf710795c1b4e85afd
n-ravi <n-ravi@users.noreply.github.com>
2015-05-18 10:16:02 -0700

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,8 +1,12 @@
 ## Create one R script called run_analysis.R that does the following:
 ## 1. Merges the training and the test sets to create one data set.
+
 ## 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+
 ## 3. Uses descriptive activity names to name the activities in the data set
+
 ## 4. Appropriately labels the data set with descriptive activity names.
+
 ## 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
 if (!require("data.table")) {
@@ -16,10 +20,10 @@ if (!require("reshape2")) {
 require("data.table")
 require("reshape2")
 
-# Load: activity labels
+# Load: Activity labels
 activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")[,2]
 
-# Load: data column names
+# Load: Data column names
 features <- read.table("./UCI HAR Dataset/features.txt")[,2]
 
 # Extract only the measurements on the mean and standard deviation for each measurement.
